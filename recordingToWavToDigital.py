b89effac7e4e444cdce89f641c9f290801b5aefe
ClaudiaMarchPiris <32073094+ClaudiaMarchPiris@users.noreply.github.com>
2019-03-05 13:25:37 +0100

    recordingToWavToDigital !
    Doesn't detect any sound on my computer

---
+++ recordingToWavToDigital.py
@@ -0,0 +1,61 @@
+# Recording to Digital 
+
+import pyaudio
+import wave
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 30
+WAVE_OUTPUT_FILENAME = "file_x.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print("recording...")
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print("finished recording")
+
+ 
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+
+
+
+
+# .wav to Digital
+
+from pydub import AudioSegment
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+sound = AudioSegment.from_wav("file_x.wav")
+
+a = np.array(sound.get_array_of_samples())
+
+plt.plot(a)
+plt.show()
+
+print("Array:", a)
+print("Max:", a.max())
+print("Shape:", a.shape)
+ 
+
