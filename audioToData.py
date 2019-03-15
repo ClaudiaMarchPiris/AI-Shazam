4fe6e86b360a9d55dcdeccdbd0a0e0ebb52d1795
williamkalin <31542574+williamkalin@users.noreply.github.com>
2019-03-15 19:28:15 +0100

    Update audioToData.py

--- audioToData.py
+++ audioToData.py
@@ -2,39 +2,48 @@ import numpy as np
 from makeSongs import *
 from mp3ToDigital import *
 
-# constants
-aFreq = 44100
-sampleSize = 4096
-parts = [0,20,40,80,160,320]
-npart = 20
-fftfreqs = np.fft.rfftfreq(sampleSize, 1/aFreq)
 
-# utils
-def partition(rfft):
-	x = []
-	for i in range(len(parts)-1):
-		boxsize = (parts[i+1]-parts[i])//npart
-		x.append(sum((rfft[parts[i]+j:parts[i+1]:boxsize] for j in range(boxsize))))
-	return np.concatenate(x)
-def partitions():
-	x = []
-	for i in range(len(parts)-1):
-		boxsize = (parts[i+1]-parts[i])//npart
-		x.append(fftfreqs[parts[i]:parts[i+1]:boxsize])
-	return np.concatenate(x)
+aFreq = 44100						# Assign  constants.
+sampleSize = 4096					#
+parts = [0,20,40,80,160,320]				# 
+npart = 20						#
+fftfreqs = np.fft.rfftfreq(sampleSize, 1/aFreq)		#
 
 
-#generate rfft and partitions
+def partition(rfft):										
+	x = []										     # Create array-holders for the partitioning  of 
+	
+	for i in range(len(parts)-1):							     # Goes though the different frequency ranges
+		boxsize = (parts[i+1]-parts[i])//npart					     # and assigns them to equaly sized ranges of 20
+		x.append(sum((rfft[parts[i]+j:parts[i+1]:boxsize] for j in range(boxsize)))) #
+	
+	return np.concatenate(x)							     # concatenate the results to create array of lenght 100
+
+def partitions():									    
+	
+	x = []										     # Create array-holders for the partitioning.
+	
+	for i in range(len(parts)-1):							     #
+		boxsize = (parts[i+1]-parts[i])//npart					     #
+		x.append(fftfreqs[parts[i]:parts[i+1]:boxsize])				     #
+		
+	return np.concatenate(x)  							     #
+
+
 def convert(audio):
-	rffts = []
-	x = []
-	for i in range(audio.size//sampleSize):
-		data = audio[sampleSize*i:sampleSize*(i+1)]
-		rfft = np.array(np.abs(np.fft.rfft(data)))
-		# rfft[j] is the amplitude of f_j=j*(aFreq/sampleSize)Hz ~= j*10. 
-		# f_last = aFreq = 44100 Hz, which is 2-3 times higher than the highest note we can hear.
-		rffts.append(rfft)
-		x.append(partition(rfft))
+	
+	rffts = []									     # Create array-holders for the partitioning.
+	x = []										     # 
+	
+	for i in range(audio.size//sampleSize):						     
+		
+		data = audio[sampleSize*i:sampleSize*(i+1)]				     # Split the data up in chunks
+		
+		rfft = np.array(np.abs(np.fft.rfft(data)))				     # Use the numpy library to calculate the fast fourier transform
+		
+		rffts.append(rfft)							     # 
+		x.append(partition(rfft))						     #
+		
 	return np.array(rffts), np.array(x)
 
 
