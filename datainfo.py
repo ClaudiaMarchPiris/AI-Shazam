518b739010da8b15cd91ff3585b761f91a565d40
williamkalin <31542574+williamkalin@users.noreply.github.com>
2019-03-18 15:28:07 +0100

    Update datainfo.py

--- datainfo.py
+++ datainfo.py
@@ -5,24 +5,24 @@ songs = 30
 
 ###################################################################################################################################
 
-class KerasBatchGenerator(object): 				# Creates an object as an interface between data and keras fit_generator
+class KerasBatchGenerator(object): 				# Creates an object as an interface between data and keras fit_generator.
 	def __init__(self, data, labs, batch_size, num_steps):
 		self.data = data
 		self.labs = labs
 		self.num_steps = num_steps
 		self.batch_size = batch_size
-		self.current_idx = 0				# current_idx will track the progress of the batches sequentially through the
+		self.current_idx = 0				# Current_idx will track the progress of the batches sequentially through the
 								# data set - once the data reaches the end of the data set it will reset
-								# back to zero
+								# back to zero.
 		
-	def generate(self): 					# Returns a batch_size long list of sequences of length num_steps
+	def generate(self): 					# Returns a batch_size long list of sequences of length num_steps.
 		x = np.zeros((self.batch_size, 
 			      self.num_steps, 
 			      self.data.shape[1]))
 		y = np.zeros((self.batch_size, self.num_steps, songs))
 		while True:
 			for i in range(self.batch_size):
-								# reset the index back to the start of the data set if we reach the end
+								# Reset the index back to the start of the data set if we reach the end.
 				if self.current_idx + self.num_steps > len(self.data):
 					self.current_idx = 0
 				x[i, :] = self.data[self.current_idx:self.current_idx + self.num_steps]
@@ -32,7 +32,7 @@ class KerasBatchGenerator(object): 				# Creates an object as an interface betwe
 
 ###################################################################################################################################
 
-def getData(): 						# Load data into numpy array and return
+def getData(): 						# Load data into numpy array and return.
 	with open("data\\xs.pkl", "rb") as file:
 		x = pk.load(file)
 	with open("data\\ys.pkl", "rb") as file:
@@ -47,14 +47,14 @@ def getData(): 						# Load data into numpy array and return
 
 ##################################################################################################################################
 	
-def getRandomChunks(n, l): 				# create random sequence of tests through n sequences of length l*batch_size
+def getRandomChunks(n, l): 				# Create random sequence of tests through n sequences of length l*batch_size
 	x, y, _ = getData()				# concatenated into single matrix.
 	gen = KerasBatchGenerator(x, y, batch_size, num_steps)
 	tests = []
 	facit = []
 	for i in range(n):
 		for j in range(randint(0,500)):
-			next(gen.generate()) #skip random amount of batches
+			next(gen.generate()) 		# Skip random amount of batches.
 		for j in range(l):
 			x,y = next(gen.generate())
 			tests.append(x)
